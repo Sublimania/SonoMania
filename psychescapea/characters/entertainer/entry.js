1ec186c69b2a5cdd5c16ab53410d3de315d474f1
Lucif Crøs <sonomania@email.com>
2026-08-08 22:52:08 -0400

    Update entry.js

--- psychescapea/characters/entertainer/entry.js
+++ psychescapea/characters/entertainer/entry.js
@@ -1,7 +1,7 @@
 function confirmEntry() {
     const confirm = document.getElementById('enter-popup');
 
-    const background_music = new Audio('tijuana sound machine.mp3');
+    const background_music = new Audio('tijuana_sound_machine.mp3');
     background_music.play();
     background_music.loop = true;
 
