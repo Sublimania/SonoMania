348e1b72d05acaaf9520654eacbd3fb9736f3ff9
Lucif Crøs <sonomania@email.com>
2026-08-10 16:32:36 -0400

    Add files via upload

--- psychescapea/characters/entertainer/entry.js
+++ psychescapea/characters/entertainer/entry.js
@@ -4,6 +4,8 @@ function confirmEntry() {
     const background_music = new Audio('tijuana_sound_machine.mp3');
     background_music.play();
     background_music.loop = true;
-
+    document.title = "Tacos Con Espagueti!";
+    const icon = document.getElementById('favicon');
+    icon.href = 'https://sonomania.forgeheart.org/psychescapea/favicon.ico'
     confirm.remove();
 }
